26cbf9962f5530078b15d54dcc2981a90d7a71a5
Gawri Edussuriya <gawriedussuriya.edussuriya@unimelb@edu.au>
2019-06-28 21:33:20 +1000

    UI structure

---
+++ src/MatchUI.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import  './styles/styles.scss';
+
+export default class MatchUI extends Component {
+
+    render() {
+        return (
+            <>
+                <div>Hello world</div>
+            </>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './styles/styles.scss';
+import MatchUI from './MatchUI';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main = (props) => (
+    <BrowserRouter>
+        <div className="mainContainer">
+            <div className="headerContainer"></div>
+            <div className="bodyContainer">
+                <Route path="/" exact component={MatchUI} />
+            </div>
+            <div className="footerContainer">Tennis score keeper</div>
+        </div>
+    </BrowserRouter>
+)
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
